b018c4e96781d8316ae3a67d23e63a301eedf5c3
Dmitry Razumov <razumov_d@ddc.by>
2025-07-29 10:11:50 +0300

    chore: install eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,10 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -11,6 +15,38 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ['./src/**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'no-undef': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'semi': ['off'],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^_$',
+          argsIgnorePattern: '^_$',
+        },
+      ],
+      'max-len': [
+        'warn',
+        {
+          code: 180,
+          ignoreUrls: true,
+          ignoreComments: false,
+        },
+      ],
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+    },
+  }
 ];
 
 export default eslintConfig;
